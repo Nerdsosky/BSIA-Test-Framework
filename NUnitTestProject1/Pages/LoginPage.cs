dcbc8c73a808985d2acc61c235a6270ef6d28603
Nerdsosky <jwysosky@student.umuc.edu>
2015-11-16 16:54:21 -0500

    Added LoginPage functionality
    I added some methods and elements to the LoginPage file.

--- NUnitTestProject1/Pages/LoginPage.cs
+++ NUnitTestProject1/Pages/LoginPage.cs
@@ -17,5 +17,54 @@ namespace NUnitTestProject1.Pages
         {
             this.driver = driver;
         }
+
+        By emailInput = By.Id("MainContent_Email");
+        By passwordInput = By.Id("MainContent_Password");
+        By submitButton = By.Name("ctl00$MainContent$ctl05");
+        By rememberMe = By.Id("MainContent_RememberMe");
+        By registerMeLink = By.Id("MainContent_RegisterHyperLink");
+
+        public LoginPage EnterEmail(string email)
+        {
+            driver.FindElement(emailInput).SendKeys(email);
+            return this;
+        }
+
+        public LoginPage EnterPassword(string password)
+        {
+            driver.FindElement(passwordInput).SendKeys(password);
+            return this;
+        }
+
+        public LoginPage ClickRememberMe()
+        {
+            driver.FindElement(rememberMe).Click();
+            return this;
+        }
+
+        public HomePage SubmitLogin()
+        {
+            driver.FindElement(submitButton).Click();
+            return new HomePage(driver);
+        }
+
+        public LoginPage SubmitLoginExpectingError()
+        {
+            driver.FindElement(submitButton).Click();
+            return this;
+        }
+
+        public HomePage LoginUser(string email, string password)
+        {
+            EnterEmail(email);
+            EnterPassword(password);
+            return SubmitLogin();
+        }
+
+        public RegisterPage ClickRegisterMeLink()
+        {
+            driver.FindElement(registerMeLink).Click();
+            return new RegisterPage(driver);
+        }
     }
 }
