e00fd05d86e751ced3d0a050d304224adaa1c240
Nerdsosky <jwysosky@student.umuc.edu>
2015-11-25 12:43:04 -0500

    updated creatpage object and created createpagetest file

--- NUnitTestProject1/Pages/CreatePage.cs
+++ NUnitTestProject1/Pages/CreatePage.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Firefox;
+using System.IO;
 
 namespace NUnitTestProject1.Pages
 {
@@ -17,5 +18,60 @@ namespace NUnitTestProject1.Pages
         {
             this.driver = driver;
         }
+
+        //Bus Information
+        By busDropdown = By.Id("MainContent_ddl_bus");
+        By seasonDropdown = By.Id("MainContent_ddl_season");
+        By inspectionDateTextbox = By.Id("MainContent_txt_inspectionDate");
+        By getBusInfoButton = By.Id("MainContent_btn_getBus");
+
+        //Create Inspection
+        By odometerTextbox = By.Id("MainContent_txt_odometer");
+        By tagTextbox = By.Id("MainContent_txt_tag");
+        By createdByTextbox = By.Id("MainContent_txt_createdBy");
+        By upadteByTextbox = By.Id("MainContent_txt_updatedBy");
+
+
+        public IList<IWebElement> GetGroupInputElements(int groupNumber)
+        {
+            driver.FindElement(By.PartialLinkText("Group " + groupNumber)).Click();
+
+            IList<IWebElement> inputChildElements = new List<IWebElement>();
+            IWebElement group = driver.FindElement(By.Id("div_" + (groupNumber - 1)));
+            IList<IWebElement> groupChildElements = group.FindElements(By.XPath(".//*"));
+
+            foreach(IWebElement item in groupChildElements)
+            {
+                if(item.TagName.Equals("input")){
+                    inputChildElements.Add(item);
+                    Console.WriteLine("added " + item.GetAttribute("type"));
+                 }
+
+             }
+            
+
+            return inputChildElements;
+        }
+
+        public CreatePage SelectBus(string busNumber)
+        {
+            SelectElement select = new SelectElement(driver.FindElement(busDropdown));
+            select.SelectByText(busNumber);
+            return this;
+        }
+
+        public CreatePage SelectSeason(string season)
+        {
+            SelectElement select = new SelectElement(driver.FindElement(seasonDropdown));
+            select.SelectByText(season);
+            return this;
+        }
+
+        public CreatePage ClickGetBusInfo()
+        {
+            driver.FindElement(getBusInfoButton).Click();
+            return this;
+        }
+
     }
 }
---
+++ NUnitTestProject1/Tests/CreatePageTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using NUnitTestProject1.Pages;
+using System.Collections.Generic;
+
+namespace NUnitTestProject1.Tests
+{
+    [TestFixture]
+    public class CreatePageTests
+    {
+        private string applicationURL = "http://bsia.azurewebsites.net/create";
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Init()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(3));
+            driver.Navigate().GoToUrl(applicationURL);
+        }
+
+        [Test]
+        public void CheckGroupInputs([Values(1)] int groupNumber)
+        {
+            CreatePage createPage = new CreatePage(driver);
+
+            createPage.SelectBus("1");
+            createPage.SelectSeason("Fall");
+            createPage.ClickGetBusInfo();
+
+
+
+            IList<IWebElement> inputItems = createPage.GetGroupInputElements(groupNumber);
+            Console.WriteLine("Got Input Items" + driver.Title);
+            foreach (IWebElement item in inputItems)
+            {
+                Console.WriteLine("Inside Foreach" + driver.Title);
+                switch (item.GetAttribute("type"))
+                {
+                    case "checkbox":
+                        item.SendKeys("\n");
+                        break;
+                    case "text":
+                        item.SendKeys("Testing");
+                        Console.WriteLine("Typed in " + item.GetAttribute("id"));
+                        break;
+                }
+            }
+
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
